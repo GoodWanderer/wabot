db502092a72f7bb01b0a6489a2e20513bb7a092e
Макар <goodwanderer@outlook.com>
2020-11-19 08:15:16 +0500

    test4

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 def home():
     if request.method == 'POST':
         bot = WABot(request.json)
-        print("Тут тип что то будет:\n\n"+bot+"\n\n")
+        print("Тут тип что то будет:\n\n"+str(bot)+"\n\n")
         return bot.processing()
 
 if(__name__) == '__main__':
