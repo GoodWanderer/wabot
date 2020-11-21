a38bb2d691606a970f5ee0103fa15e9ec9969a88
Макар <goodwanderer@outlook.com>
2020-11-21 20:09:01 +0500

    time25

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask, request, jsonify
 from wabot import WABot
 import json
 
+from time import sleep
+
 app = Flask(__name__)
 
 a = True
 while a:
+    sleep(3)
     print(1)
 
 @app.route('/', methods=['POST'])
