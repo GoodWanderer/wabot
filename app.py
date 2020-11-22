bc98ed0163b1956a8181514e7c38ca72c0fb134d
Макар <goodwanderer@outlook.com>
2020-11-22 20:16:29 +0500

    time47

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ from flask import Flask, request, jsonify
 from wabot import WABot, sendmessage
 import json
 
-from time import sleep
+import time
+from timeloop import Timeloop
+from datetime import timedelta
+
 
 app = Flask(__name__)
 
@@ -11,13 +14,17 @@ app = Flask(__name__)
 #     sleep(3)
 #     print(1)
 
+tl = Timeloop()
+
+@tl.job(interval=timedelta(seconds=2))
+def sample_job_every_2s():
+    print("2s job current time : {}".format(time.ctime()))
+
 @app.route('/', methods=['POST'])
 def home():
     if request.method == 'POST':
         bot = WABot(request.json)
         return bot.processing()
-    else:
-        sendmessage()
 
 if(__name__) == '__main__':
     app.run()
