82cc38eed5a2d458519da7e831df1fd6352ccd49
Макар <goodwanderer@outlook.com>
2020-11-22 20:52:28 +0500

    time53

--- app.py
+++ app.py
@@ -2,8 +2,10 @@ from flask import Flask, request, jsonify
 from wabot import WABot
 import json
 
-app = Flask(__name__)
+import time
+import threading
 
+app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def home():
@@ -11,5 +13,21 @@ def home():
         bot = WABot(request.json)
         return bot.processing()
 
-if(__name__) == '__main__':
-    app.run()
+def sheduler(n):
+  _time = time.time()
+  print('start loop', n)
+  while True:
+    if _time + 1 < time.time():
+      print(n, ':', time.time())
+      _time = time.time()
+
+    time.sleep(0.1)
+
+th1 = threading.Thread(target=app.run)
+th2 = threading.Thread(target=sheduler, args=(2,))
+
+th1.start()
+th2.start()
+
+# if(__name__) == '__main__':
+#     app.run()
