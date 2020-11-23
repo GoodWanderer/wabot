c6469224f4641a866b3c301ae204e46d6e141500
Макар <goodwanderer@outlook.com>
2020-11-23 10:58:26 +0500

    time68

--- wabot.py
+++ wabot.py
@@ -96,7 +96,7 @@ class WABot():
     def processing(self):
         if self.dict_messages != []:
             for message in self.dict_messages:
-                print(message)
+                print("\n\nmessage\n\n")
                 if not message['fromMe']:
                     text = message['body']
                     id = message['chatId']
